3154ef3bb6372c02540c7b8c6a05d68a48166459
tartebabba <jez.piedad@gmail>
2024-05-15 17:03:22 +0100

    updated loading screen

--- src/components/Screens.jsx
+++ src/components/Screens.jsx
@@ -1,7 +1,24 @@
+import { Progress } from "@/components/ui/progress"
+import { useState, useEffect } from "react"
+import { ring } from 'ldrs'
+ring.register()
+
 export default function LoadingScreen() {
+  const [progress, setProgress] = useState(13)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500)
+    return () => clearTimeout(timer)
+  }, [])
   return (
-    <div>
-      <p>loading</p>
+    <div className='flex items-center justify-center min-h-screen'>
+      <l-ring
+        size="100"
+        stroke="5"
+        bg-opacity="0"
+        speed="2" 
+        color="black"
+      ></l-ring>
     </div>
   );
 }
