7eb0e818db0c74f6ca4586924dace5c502f48152
tartebabba <83190958+tartebabba@users.noreply.github.com>
2024-06-12 10:06:21 +0100

    center select user component

--- src/components/users/account-page.jsx
+++ src/components/users/account-page.jsx
@@ -4,16 +4,13 @@ import { UserContext, UserUpdateContext } from '@/components/Context';
 import { Link } from 'react-router-dom';
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
 
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 
 export default function AccountPage() {
   const user = useContext(UserContext);
@@ -25,16 +22,16 @@ export default function AccountPage() {
   }, [user]);
   return (
     <>
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+      <main className="bg-muted/40 flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
         <div className="mx-auto grid w-full max-w-6xl gap-2">
           <h1 className="text-3xl font-semibold">Welcome, {user.username}!</h1>
         </div>
         <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
           <nav
-            className="grid gap-4 text-sm text-muted-foreground"
+            className="text-muted-foreground grid gap-4 text-sm"
             x-chunk="dashboard-04-chunk-0"
           >
-            <Link href="#" className="font-semibold text-primary">
+            <Link href="#" className="text-primary font-semibold">
               General
             </Link>
           </nav>
