823112561f898bfbd0094b72c73c1c767d272004
tartebabba <jez.piedad@gmail>
2024-07-08 19:22:28 +0100

    add icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,27 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <>
-      <header className="bg-red-600 py-2 text-center">
+      <header className="relative flex items-center bg-red-600 py-2">
         <Link
-          to="/"
-          id="site-name"
-          className="font-mono font-bold text-slate-50"
+          to="https://www.jezpie.dev/"
+          id="personal-logo"
+          className="absolute left-2"
         >
-          Fakeddit
+          <img
+            src="../public/JP-icon-32x32.png"
+            alt="personal logo"
+            className="h-4 w-4 rounded-md hover:bg-red-500"
+          />
         </Link>
+        <div className="flex-grow text-center">
+          <Link
+            to="/"
+            id="site-name"
+            className="font-mono font-bold text-slate-50"
+          >
+            Fakeddit
+          </Link>
+        </div>
       </header>
     </>
   );
